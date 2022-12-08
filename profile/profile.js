b9290e78b2322d4379d53440985fadbe07973dfe
Brian <bellbrian053@gmail.com>
2022-12-08 00:22:26 -0500

    complete

--- profile/profile.js
+++ profile/profile.js
@@ -1,2 +1,14 @@
 const UserData=getLoginData()
 console.log(UserData)
+
+/*sidebar*/
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
